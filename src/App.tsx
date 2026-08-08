9bcc2dabf578c1462c2d3699c6673829c9fc662c
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-08 17:51:23 +0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Header from "./components/Header/Header";
+
 export default function App() {
   return (
     <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        <span className="header__connect">Connect</span>
+        <span className="header__four">Four</span>
+      </h1>
+    </header>
+  );
+}
