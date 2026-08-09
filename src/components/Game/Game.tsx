5da26acb963c2c46268a047abc4c096fafac8721
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-09 21:20:22 +0300

    feat: add game state model

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
+
 type Player = "red" | "yellow";
 type Cell = Player | null;
 type Board = Cell[][];
 
+type Status =
+  | { type: "playing" }
+  | { type: "won", winner: Player }
+  | { type: "draw" };
+
 type Game = {
   board: Board;
+  currentPlayer: Player;
+  status: Status;
 };
 
 const cols = 7;
 const rows = 6;
 
 export default function Game() {
+  const [game, setGame] = useState<Game>(createGame);
+
   return (
     <div className="board">
     </div>
   );
+}
+
+function createGame(): Game {
+  return {
+    board: createBoard(),
+    currentPlayer: "red",
+    status: { type: "playing" }
+  };
 }
 
 function createBoard(): Board {
