6344412ec484a8a177ebf4851b120d7c74b64485
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-11 20:24:17 +0300

    feat: add game board controls

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import "./Game.css";
+import sprite from "../../assets/icons/sprite.svg?no-inline";
 
 type Player = "red" | "yellow";
 type Cell = Player | null;
@@ -25,6 +26,18 @@ export default function Game() {
 
   return (
     <div className="board">
+      <div className="board__controls">
+        {Array.from({ length: cols }, (_, colIndex) => (
+          <div key={colIndex} className="board__column">
+            <button className="board__btn">
+              <svg className="board__arrow">
+                <use href={`${sprite}#arrow`} />
+              </svg>
+            </button>
+          </div>
+        ))}
+      </div>
+
       <div className="board__grid">
         {game.board.map((row, rowIndex) => row.map((cell, colIndex) => (
           <div key={`${rowIndex}-${colIndex}`} className="board__cell">
