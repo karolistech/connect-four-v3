99c68babdfda4e7950cf07f2a3752f8520eb61a6
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-12 16:37:19 +0300

    feat: add game disc drop handling

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -24,12 +24,16 @@ const rows = 6;
 export default function Game() {
   const [game, setGame] = useState<Game>(createGame);
 
+  function handleDropDisc(col: number) {
+    setGame(game => dropDisc(game, col));
+  }
+
   return (
     <div className="board">
       <div className="board__controls">
         {Array.from({ length: cols }, (_, colIndex) => (
           <div key={colIndex} className="board__column">
-            <button className="board__btn">
+            <button className="board__btn" onClick={() => handleDropDisc(colIndex)}>
               <svg className="board__arrow">
                 <use href={`${sprite}#arrow`} />
               </svg>
@@ -58,4 +62,23 @@ function createGame(): Game {
 
 function createBoard(): Board {
   return Array.from({ length: rows }, () => Array(cols).fill(null));
+}
+
+function dropDisc(game: Game, col: number): Game {
+  const board = game.board.map(row => [...row]);
+  const player = game.currentPlayer;
+
+  for (let row = rows - 1; row >= 0; row--) {
+    if (board[row][col] !== null) continue;
+
+    board[row][col] = player;
+
+    return {
+      status: { type: "playing" },
+      board: board,
+      currentPlayer: player === "red" ? "yellow" : "red"
+    };
+  }
+
+  return game;
 }
