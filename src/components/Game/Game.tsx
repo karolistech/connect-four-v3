c2092e7123c90be1e82fd1388b5e74c7fcd41485
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-08-14 14:50:45 +0300

    feat: add game board size modes

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -7,31 +7,63 @@ type Player = "red" | "yellow";
 type Cell = Player | null;
 type Board = Cell[][];
 
+type Mode = "7x6" | "8x7" | "9x8";
+
 type Status =
   | { type: "playing" }
   | { type: "won", winner: Player }
   | { type: "draw" };
 
 type Game = {
+  mode: Mode;
+  status: Status;
   board: Board;
   currentPlayer: Player;
-  status: Status;
 };
 
-const cols = 7;
-const rows = 6;
+type BoardSize = { cols: number; rows: number };
+
+const boardSizes: Record<Mode, BoardSize> = {
+  "7x6": { cols: 7, rows: 6 },
+  "8x7": { cols: 8, rows: 7 },
+  "9x8": { cols: 9, rows: 8 }
+};
 
 export default function Game() {
-  const [game, setGame] = useState<Game>(createGame);
+  const [game, setGame] = useState<Game>(() => createGame("7x6"));
+
+  const boardSize = boardSizes[game.mode];
 
   function handleDropDisc(col: number) {
     setGame(game => dropDisc(game, col));
   }
 
+  function newGame() {
+    setGame(createGame(game.mode));
+  }
+
   return (
     <div className="board">
-      <div className="board__controls">
-        {Array.from({ length: cols }, (_, colIndex) => (
+      <div className="board__modes">
+        <button onClick={newGame}>
+          New Game
+        </button>
+
+        <button onClick={() => setGame(createGame("7x6"))}>
+          7 x 6
+        </button>
+
+        <button onClick={() => setGame(createGame("8x7"))}>
+          8 x 7
+        </button>
+
+        <button onClick={() => setGame(createGame("9x8"))}>
+          9 x 8
+        </button>
+      </div>
+
+      <div className={`board__controls board__controls--${game.mode}`}>
+        {Array.from({ length: boardSize.cols }, (_, colIndex) => (
           <div key={colIndex} className="board__column">
             <button className="board__btn" onClick={() => handleDropDisc(colIndex)}>
               <svg className="board__btn-icon">
@@ -42,7 +74,7 @@ export default function Game() {
         ))}
       </div>
 
-      <div className="board__grid">
+      <div className={`board__grid board__grid--${game.mode}`}>
         {game.board.map((row, rowIndex) => row.map((cell, colIndex) => (
           <div key={`${rowIndex}-${colIndex}`} className="board__cell">
             {cell !== null && (
@@ -57,31 +89,35 @@ export default function Game() {
   );
 }
 
-function createGame(): Game {
+function createGame(mode: Mode): Game {
   return {
-    board: createBoard(),
-    currentPlayer: "red",
-    status: { type: "playing" }
+    mode: mode,
+    status: { type: "playing" },
+    board: createBoard(boardSizes[mode]),
+    currentPlayer: "red"
   };
 }
 
-function createBoard(): Board {
-  return Array.from({ length: rows }, () => Array(cols).fill(null));
+function createBoard(boardSize: BoardSize): Board {
+  return Array.from({ length: boardSize.rows }, () => Array(boardSize.cols).fill(null));
 }
 
 function dropDisc(game: Game, col: number): Game {
   if (game.status.type !== "playing") return game;
 
+  const mode = game.mode;
+  const boardSize = boardSizes[game.mode];
   const board = game.board.map(row => [...row]);
   const player = game.currentPlayer;
 
-  for (let row = rows - 1; row >= 0; row--) {
+  for (let row = boardSize.rows - 1; row >= 0; row--) {
     if (board[row][col] !== null) continue;
 
     board[row][col] = player;
 
     if (connectFour(board, row, col, player) === true) {
       return {
+        mode: mode,
         status: { type: "won", winner: player },
         board: board,
         currentPlayer: player
@@ -90,6 +126,7 @@ function dropDisc(game: Game, col: number): Game {
 
     else if (boardFull(board) === true) {
       return {
+        mode: mode,
         status: { type: "draw" },
         board: board,
         currentPlayer: player
@@ -98,6 +135,7 @@ function dropDisc(game: Game, col: number): Game {
 
     else {
       return {
+        mode: mode,
         status: { type: "playing" },
         board: board,
         currentPlayer: player === "red" ? "yellow" : "red"
